0f8ebfb6366034a09a05f45558584d6d3a063439
Jose-Mayhuasca <josedanielmayhuasca@gmail.com>
2025-10-15 23:21:27 -0500

    fix: change 2 index

--- src/components/WebSite/HomeSections/HeroSectionOne.vue
+++ src/components/WebSite/HomeSections/HeroSectionOne.vue
@@ -1,11 +1,11 @@
 <template>
-    <h1>Aca es donde vas a crear todas las secciones para esta pagina, y asi con las demas paginas en sus respectivas
+    <h1 class="bg-blue-500">Aca es donde vas a crear todas las secciones para esta pagina, y asi con las demas paginas
+        en sus respectivas
         carpetas broer</h1>
     <Button label="¡Se supone que ahora si se puede usar el prime vue con normalidad!" class="cta"
         icon="pi pi-arrow-right" iconPos="right" />
 
 </template>
-
 
 /**
 * *Importante: Falta definir contenedores por eso se ve el boton tan ancho xD
