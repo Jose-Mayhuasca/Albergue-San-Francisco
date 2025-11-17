82632e47c159f2025e7402da974a3743f0a58f56
Jose-Mayhuasca <josedanielmayhuasca@gmail.com>
2025-11-17 10:48:21 -0500

    feat: update phone number

--- src/layout/WebSite/AppFooter.vue
+++ src/layout/WebSite/AppFooter.vue
@@ -17,7 +17,7 @@
       <div>
         <h2 class="text-xl font-semibold mb-3">Contáctanos</h2>
         <ul class="space-y-2 text-gray-300 text-sm">
-          <li><i class="ri-phone-fill mr-2"></i> +51 123 456 789</li>
+          <li><i class="ri-whatsapp-fill mr-2"></i> +51 922 266 310</li>
         </ul>
       </div>
 
@@ -25,10 +25,15 @@
       <div>
         <h2 class="text-xl font-semibold mb-3">Síguenos</h2>
         <div class="flex space-x-4 text-2xl">
-          <a href="https://www.facebook.com/alberguesanfrancisco?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer" class="hover:text-gray-400 transition"><i class="ri-facebook-circle-fill"></i></a>
-          <a href="https://www.instagram.com/albergue.sanfrancisco/" target="_blank" rel="noopener noreferrer" class="hover:text-gray-400 transition"><i class="ri-instagram-fill"></i></a>
-          <a href="https://www.tiktok.com/@stephanie_mv11?_r=1&_t=ZS-91SWCKTVjWx" target="_blank" rel="noopener noreferrer" class="hover:text-gray-400 transition"><i class="ri-tiktok-fill"></i></a>
-          <a href="https://api.whatsapp.com/send/?phone=51951703662&text=Hola%2C+quiero+m%C3%A1s+informaci%C3%B3n+sobre+adopciones&type=phone_number&app_absent=0" target="_blank" rel="noopener noreferrer" class="hover:text-gray-400 transition"><i class="ri-whatsapp-fill"></i></a>
+          <a href="https://www.facebook.com/alberguesanfrancisco?mibextid=ZbWKwL" target="_blank"
+            rel="noopener noreferrer" class="hover:text-gray-400 transition"><i class="ri-facebook-circle-fill"></i></a>
+          <a href="https://www.instagram.com/albergue.sanfrancisco/" target="_blank" rel="noopener noreferrer"
+            class="hover:text-gray-400 transition"><i class="ri-instagram-fill"></i></a>
+          <a href="https://www.tiktok.com/@stephanie_mv11?_r=1&_t=ZS-91SWCKTVjWx" target="_blank"
+            rel="noopener noreferrer" class="hover:text-gray-400 transition"><i class="ri-tiktok-fill"></i></a>
+          <!-- <a href="https://api.whatsapp.com/send/?phone=51951703662&text=Hola%2C+quiero+m%C3%A1s+informaci%C3%B3n+sobre+adopciones&type=phone_number&app_absent=0"
+            target="_blank" rel="noopener noreferrer" class="hover:text-gray-400 transition"><i
+              class="ri-whatsapp-fill"></i></a> -->
         </div>
       </div>
 
@@ -55,7 +60,6 @@
     padding-bottom: 60px;
   }
 }
-
 </style>
 
 //si mi delegeit
